8c3b7af66dcd0c0f8f110121f5e11d2cf5aea58c
Himanshu Sangale <himanshu@Himanshus-Air.lan>
2024-02-01 17:09:00 +0530

    Added Custom CSS, Page templates and Title to page

--- functions.php
+++ functions.php
@@ -9,3 +9,11 @@ function university_files() {
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
+
+function university_features(){
+  // To add Title to page/browser tab
+  add_theme_support('title-tag');
+}
+
+
+add_action('after_setup_theme', 'university_features');
