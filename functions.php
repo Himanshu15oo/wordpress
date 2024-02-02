ea2cb94f83f4fd767e53e3426727da9e99d542c2
Himanshu Sangale <himanshu@Himanshus-Air.lan>
2024-02-02 17:25:13 +0530

    Added post type - Program and its related pages

--- functions.php
+++ functions.php
@@ -29,6 +29,12 @@ function university_adjust_queries($query)
 {
   $today = date('Ymd');
 
+  if (!is_admin() and is_post_type_archive('program') and $query->is_main_query()) {
+    $query->set('orderby', 'title');
+    $query->set('order', 'ASC');
+    $query->set('posts_per_page', -1);
+  }
+
   if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
     $query->set('meta_key', 'event_date');
     $query->set('orderby', 'meta_value_num');
--- single-event.php
+++ single-event.php
@@ -26,6 +26,25 @@ while (have_posts()) {
             <?php the_content(); ?>
         </div>
 
+        <?php
+        $relatedPrograms = get_field('related_programs');
+
+        if ($relatedPrograms) {
+            echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+            echo '<ul class="link-list min-list">';
+            foreach ($relatedPrograms as $program) {
+        ?>
+                <li><a href="<?php echo get_the_permalink($program) ?>"><?php echo $program->post_title; ?></a></li>
+        <?php
+                // echo $program['post_title'];
+
+            }
+            echo '</ul>';
+        }
+
+        ?>
+
     </div>
 
 <?php }
