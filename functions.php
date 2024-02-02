bef3f6e165717484566758226a8c19fff5809632
Himanshu Sangale <himanshu@Himanshus-Air.lan>
2024-02-02 09:28:13 +0530

    Separate page for Home and Blog listing

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
-function university_files() {
+function university_files()
+{
   wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
@@ -10,9 +11,14 @@ function university_files() {
 
 add_action('wp_enqueue_scripts', 'university_files');
 
-function university_features(){
+function university_features()
+{
   // To add Title to page/browser tab
   add_theme_support('title-tag');
+  // To add menu support
+  register_nav_menu('header', 'Header Menu');
+  register_nav_menu('footer1', 'Footer 1');
+  register_nav_menu('footer2', 'Footer 2');
 }
 
 
