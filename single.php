a6f779ddeaeb8979cf53c4274d49a7f498706571
Himanshu Sangale <himanshu@Himanshus-Air.lan>
2024-02-05 08:25:56 +0530

    Minor changes to metabox in single.php

--- single.php
+++ single.php
@@ -17,20 +17,17 @@ while (have_posts()) {
 
   <div class="container container--narrow page-section">
 
+    <div class="metabox metabox--position-up metabox--with-home-link">
+      <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('post'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to Blogs</a>
+        <span class="metabox__main">
+          Posted by <?php the_author_posts_link(); ?> on <?php the_time('j/n/y'); ?> under <?php echo get_the_category_list('|'); ?>
+        </span>
+      </p>
+    </div>
+
+
     <?php
     $parent = wp_get_post_parent_id(get_the_ID());
-    if ($parent) {
-    ?>
-      <div class="metabox metabox--position-up metabox--with-home-link">
-        <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($parent) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parent); ?></a> <span class="metabox__main"><?php echo the_title(); ?></span></p>
-      </div>
-    <?php
-    }
-    ?>
-
-
-
-    <?php
 
     $children = get_pages(array('child_of' => get_the_ID()));
 
