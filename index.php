b0f7dc6a965c95a7c67b995b04f2bc38645194dd
Himanshu Sangale <himanshu@Himanshus-Air.lan>
2024-02-05 12:58:02 +0530

    Created separate template file for Events display loop

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
 <?php
 
 get_header();
+pageBanner(array(
+  'title' => 'Welcome to the blogs!',
+  'subtitle' => 'Keep up with the latest blogs on trending tech.'
+));
 ?>
-
-<div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-  <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">Welcome to the blogs!</h1>
-    <div class="page-banner__intro">
-      <p>Keep up with your latest blogs on trending tech.</p>
-    </div>
-  </div>
-</div>
 
 <div class="container container--narrow page-section">
 
